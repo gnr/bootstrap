3a283b26d6ebe60c45b4a72e0a68b9f963c9dc4e
Gabriel Vanrenen <gabe@skimble.com>
2013-04-01 17:26:29 -0700

    updating copy script

--- copy_mobile_to_skimble.sh
+++ copy_mobile_to_skimble.sh
@@ -3,4 +3,11 @@
 cd ~/dev/bootstrap
 make clean
 make bootstrap
-cp -R bootstrap/* ~/myTickList/public/bootstrap-mobile/
+cp bootstrap/js/bootstrap.js ~/myTickList/public/javascripts/bootstrap-mobile.js
+cp bootstrap/js/bootstrap.min.js ~/myTickList/public/javascripts/bootstrap-mobile.min.js
+
+cp bootstrap/css/bootstrap.css ~/myTickList/public/stylesheets/bootstrap-mobile.css
+cp bootstrap/css/bootstrap.min.css ~/myTickList/public/stylesheets/bootstrap-mobile.min.css
+cp bootstrap/css/bootstrap-responsive.css ~/myTickList/public/stylesheets/bootstrap-mobile-responsive.css
+cp bootstrap/css/bootstrap-responsive.min.css ~/myTickList/public/stylesheets/bootstrap-mobile-responsive.min.css
+
